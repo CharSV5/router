ccb815752d232669d81548e91bfc85aba381bdfc
charlene bastians <charlenebastians@hotmail.com>
2018-10-03 13:24:10 +0100

    nested routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import Category from './Category';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <nav className="navbar navbar_light">
+          <ul classname="nav navbar-nav">
+
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/category">Category</Link></li>
+            <li><Link to="/products">Products</Link></li>
+
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact={true} path="/" component={Home} />
+          <Route path="/category" component={Category} />
+          <Route path="/products" component={Products} />
+        </Switch>
+
       </div>
     );
   }
